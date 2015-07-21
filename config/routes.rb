323fa5221b1fdb3d2f3f331f4c2633453b539731
xiihu <williamramthy@hotmail.com>
2015-07-22 00:29:18 +0800

    touch up partials

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
   get   'signup'      =>  'users#signup'
 
-  get  'login'        =>  'users#login'
+  get   'login'       =>  'users#login'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
