c9e213c0de352255db3486298cb1f9b1172533ae
xiihu <williamramthy@hotmail.com>
2015-07-22 02:19:40 +0800

    until user uniqueness test on database levle

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,30 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+  	 @user = User.new(name: "huxi", email: "huxi@example.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = "     "
+    assert_not @user.valid?
+  end
+
+  test "email address should be present" do
+    @user.email = "     "
+    assert_not @user.valid?
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
 end
