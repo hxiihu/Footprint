dca9522d0d1cec4a11f3ba9d17b9a4700433142c
xiihu <williamramthy@hotmail.com>
2015-07-25 03:47:43 +0800

    Finish log in/log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,12 @@ class SessionsController < ApplicationController
       flash.now[:success] = '亲爱的，Welcome back!'
       # Log the user in and redirect to the user's show page.
       log_in user
+      #params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      if params[:session][:remember_me] == '1'
+        remember(user)
+      else
+        forget(user)
+      end
       redirect_to user
     else
       # Create an error message.
