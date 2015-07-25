3ff4674bb3ccef75ceca92a5ebc920dff0fde0fb
xiihu <williamramthy@hotmail.com>
2015-07-25 14:38:27 +0800

    requireing the right user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
       else
         forget(user)
       end
-      redirect_to user
+      redirect_back_or user
     else
       # Create an error message.
       flash.now[:danger] = 'Invalid email address or password'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   #Log in to have access
   #Profile is only accessble to user himself
   before_action :logged_in_user, only: [:edit, :update, :show]
+   before_action :correct_user,   only: [:edit, :update, :show]
 
   def signup
   	 @user = User.new
@@ -49,9 +50,16 @@ class UsersController < ApplicationController
     # Confirms a logged-in user.
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "You need to log in to view the profile."
         redirect_to login_url
       end
+    end
+
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 
 end
