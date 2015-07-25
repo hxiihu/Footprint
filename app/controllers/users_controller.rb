c7d260dc44e7b87ab6373d9ef6d80bf832f68a7d
xiihu <williamramthy@hotmail.com>
2015-07-25 19:04:46 +0800

    show all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,8 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.all
+    #@users = User.all
+    @users = User.paginate(page: params[:page])
   end
 
   def show
