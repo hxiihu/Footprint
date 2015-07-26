4106ac46785cbb84edca10b5f3011259ab83d31a
xiihu <williamramthy@hotmail.com>
2015-07-27 02:48:48 +0800

    Finish password Reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 	#attr_accessor :remember_token
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
 
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
 	validates :email, presence: true, length: { maximum: 200 }, 
 						format: { with: VALID_EMAIL_REGEX }, 
 						uniqueness: {case_sensitive: false}
-	has_secure_password
+	has_secure_password #!!!
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
 	# Returns the hash digest of the given string.
@@ -54,6 +54,23 @@ class User < ActiveRecord::Base
   # Sends activation email.
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  # Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
 
   private
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -5,7 +5,7 @@ class UserMailerTest < ActionMailer::TestCase
     user = users(:xi)
     user.activation_token = User.new_token
     mail = UserMailer.account_activation(user)
-    assert_equal "Footprint || Account activation", mail.subject
+    assert_equal "Footprint || Account Activation", mail.subject
     assert_equal [user.email], mail.to
     assert_equal ["no-reply@footprint.com"], mail.from
     #assert_match "Hi", mail.body.encoded
@@ -15,11 +15,14 @@ class UserMailerTest < ActionMailer::TestCase
   end
 
   test "password_reset" do
-    mail = UserMailer.password_reset
-    assert_equal "Password reset", mail.subject
-    assert_equal ["to@example.org"], mail.to
+    user = users(:xi)
+    user.reset_token = User.new_token
+    mail = UserMailer.password_reset(user)
+    assert_equal "Footprint || Password Reset", mail.subject
+    assert_equal [user.email], mail.to
     assert_equal ["no-reply@footprint.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_match user.reset_token,        mail.body.encoded
+    assert_match CGI::escape(user.email), mail.body.encoded
   end
 
 end
