01f11099e0320e545b5cd72a51962df10e3302a3
xiihu <williamramthy@hotmail.com>
2015-07-27 03:02:35 +0800

    Add email configuration is production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,22 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  # To configure our application to use SendGrid, we need to fill out the SMTP settings for our production environment.
+  # You will also have to define a host variable with the address of your production website.
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'yurilliam.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
