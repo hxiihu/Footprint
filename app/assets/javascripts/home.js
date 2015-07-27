144c08e50f11f53c8caf32e4ceac99490c39ebf6
xiihu <williamramthy@hotmail.com>
2015-07-27 13:02:11 +0800

    homepage animation trial #1

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,15 +1,50 @@
 
 /*Downpage Animation*/
-var main = function() {
-	
+$(document).ready(function(){
+
 	$('.dropdown-toggle').click(function(){
 		$('.dropdown-menu').toggle();
 	});
-}
+
+	$('.right-nav').click(function() {
+    var currentSlide = $('.active-slide');
+    var nextSlide = currentSlide.next();
+
+    var currentDot = $('.active-dot');
+    var nextDot = currentDot.next();
+
+    /* Until the last slide*/
+    if(nextSlide.length === 0) {
+      nextSlide = $('.slide').first();
+      nextDot = $('.dot').first();
+    }
+    
+    currentSlide.fadeOut(600).removeClass('active-slide');
+    nextSlide.fadeIn(600).addClass('active-slide');
+
+    currentDot.removeClass('active-dot');
+    nextDot.addClass('active-dot');
+	});
 
 
+	$('.left-nav').click(function() {
+    var currentSlide = $('.active-slide');
+    var prevSlide = currentSlide.prev();
 
+    var currentDot = $('.active-dot');
+    var prevDot = currentDot.prev();
 
+    if(prevSlide.length === 0) {
+      prevSlide = $('.slide').last();
+      prevDot = $('.dot').last();
+    }
+    
+    currentSlide.fadeOut(800).removeClass('active-slide');
+    prevSlide.fadeIn(800).addClass('active-slide');
 
+    currentDot.removeClass('active-dot');
+    prevDot.addClass('active-dot');
+  });
 
-$(document).ready(main);
+});
+
