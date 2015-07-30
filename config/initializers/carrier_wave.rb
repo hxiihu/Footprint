39c3b815e19e9ace85e84dff5eb79631f3bf2f05
xiihu <williamramthy@hotmail.com>
2015-07-30 17:46:50 +0800

    image upload to s3 bug fixed

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,7 +1,12 @@
+# 
+# As a web developer with computer ethics
+# Please, please pretend that you've seen nothing here
+# If I got another way to solve my upload issue, I wouldn't disclose my credentials here!
+
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
-      # Configuration for Amazon S3
+      # Configuration for S3
       :provider              => 'AWS',
       :aws_access_key_id     => 'AKIAJEAVHQOJDQ7XALQQ' , #ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => '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' #ENV['S3_SECRET_KEY']
