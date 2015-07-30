19fa7e8e234a54e2d1fdcd6f5cb5128e6e40caf9
xiihu <williamramthy@hotmail.com>
2015-07-30 22:33:06 +0800

    send email via Gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,11 +77,14 @@ Rails.application.configure do
   host = 'yurilliam.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
+    #1 :address        => 'smtp.sendgrid.net',
+    :address        => 'smtp.gmail.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    #2 :user_name      => ENV['SENDGRID_USERNAME'],
+    :user_name      => ENV['gmail_username'],
+    #3 :password       => ENV['SENDGRID_PASSWORD'],
+    :password       => ENV['gmail_password'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
